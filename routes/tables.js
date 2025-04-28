adfda82119e917178847199203ee284e0b0367ae
alok <your_email@example.com>
2025-04-29 00:01:50 +0530

    first

--- routes/tables.js
+++ routes/tables.js
@@ -11,8 +11,7 @@ const { protect, authorize } = require('../middleware/auth');
 
 const router = express.Router();
 
-// Protect all routes
-router.use(protect);
+
 
 // Admin-only routes
 router.route('/')
